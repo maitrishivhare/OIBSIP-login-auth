c42c56de9f0cce6f43bfad70a0b4f108a16c308c
maitrishivhare <maitrishivhare1@gmail.com>
2026-03-15 13:59:52 +0530

    Update server.js

--- login-auth/server.js
+++ login-auth/server.js
@@ -17,6 +17,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ✅ REGISTER Route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
 
